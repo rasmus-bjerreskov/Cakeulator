409404e41fa3136aaa2da24d6fe0a18ac28a8322
Elizabeth <elizabec@metropolia.fi>
2018-11-28 10:05:10 +0200

    Made activities

---
+++ app/src/main/java/com/example/werefrogs/cakeulator/RecipeLibrary.java
@@ -0,0 +1,13 @@
+package com.example.werefrogs.cakeulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeLibrary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_library);
+    }
+}
