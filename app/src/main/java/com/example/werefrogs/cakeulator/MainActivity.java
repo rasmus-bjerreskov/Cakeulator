99dc22c6549723b7338ec3e9c4c8a6eb630cbf7b
Elizabeth <elizabec@metropolia.fi>
2018-11-30 17:27:07 +0200

    Made main activity layout, added button logic

--- app/src/main/java/com/example/werefrogs/cakeulator/MainActivity.java
+++ app/src/main/java/com/example/werefrogs/cakeulator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.werefrogs.cakeulator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,15 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+    public void buttonPressed_toLibrary(View v) {
+        Intent intent = new Intent(this, RecipeLibraryActivity.class);
+        startActivity(intent);
+    }
+    public void buttonPressed_addToLibrary(View v) {
         Recipe r1 = new Recipe("Smørrebrød");
         r1.addIngredient(new Ingredient(1, "slice", "rye bread"));
         r1.addIngredient(new Ingredient(2, "slice", "roast beef"));
         r1.addIngredient(new Ingredient(4, "slice", "cucumber"));
         RecipeList.getInstance().addRecipe(r1);
-
-        Intent intent = new Intent(this, RecipeLibraryActivity.class);
-        startActivity(intent);
-
-
     }
 }
