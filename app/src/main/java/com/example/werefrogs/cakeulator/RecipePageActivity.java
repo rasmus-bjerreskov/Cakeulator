87e07543460d8f2c016839141fdac062b87c2d00
Rasmus <rasmusst@metropolia>
2018-12-01 17:28:53 +0200

    allowed for empty default servings field

--- app/src/main/java/com/example/werefrogs/cakeulator/RecipePageActivity.java
+++ app/src/main/java/com/example/werefrogs/cakeulator/RecipePageActivity.java
@@ -18,17 +18,14 @@ public class RecipePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_page);
-
-        Bundle b = getIntent().getExtras();
-        int i = b.getInt(EXTRA, 0);
-        recipeToPrint = RecipeList.getInstance().getRecipe(i);
-
         recipeName = findViewById(R.id.tv_recipeName);
         recipeView = findViewById(R.id.tv_recipe);
         servings = (EditText) findViewById(R.id.et_amount);
 
+        Bundle b = getIntent().getExtras();
+        int i = b.getInt(EXTRA, 0);
 
-
+        recipeToPrint = RecipeList.getInstance().getRecipe(i);
         recipeName.setText(RecipeList.getInstance().getRecipe(i).getName());
 
         servings.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -39,21 +36,25 @@ public class RecipePageActivity extends AppCompatActivity {
                 }
             }
         });
+
         updateUI();
-
-
-
     }
 
     public void updateUI() {
-        int multiplier = Integer.parseInt(servings.getText().toString()) / recipeToPrint.getServings(); // given/default servings
         String recipePrint = "";
+        int multiplier;
+
+        if (servings.getText().toString().equals(null) || servings.getText().toString().equals("")) {
+            multiplier = 1; //overrides empty inputs, activity crashes without this
+        } else {
+            multiplier = Integer.parseInt(servings.getText().toString()) / recipeToPrint.getServings(); // given/default servings
+        }
 
         for (Ingredient j : recipeToPrint.getIngredients()) {
             int newServings = j.getAmount() * multiplier;
             recipePrint += newServings + j.getStrings() + "\n";
         }
-        recipeView.setText(recipePrint);
 
+        recipeView.setText(recipePrint);
     }
 }
