0c78b3bb3639d1b6afe0fedfe6cfc26ce350ae83
Erik Ruotsalainen <erikru@metropolia.fi>
2018-12-03 14:33:14 +0200

    erik init

--- app/src/main/java/com/example/werefrogs/cakeulator/MainActivity.java
+++ app/src/main/java/com/example/werefrogs/cakeulator/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void buttonPressed_addToLibrary(View v) {
-        Recipe r1 = new Recipe("Smørrebrød");
+        Recipe r1 = new Recipe("Smørrebrd");
         r1.addIngredient(new Ingredient(1, "slice", "rye bread"));
         r1.addIngredient(new Ingredient(2, "slice", "roast beef"));
         r1.addIngredient(new Ingredient(4, "slice", "cucumber"));
