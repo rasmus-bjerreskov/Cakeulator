6aad0067299e40abeacbc2176cd0930c37e1dce6
Rasmus Bjerreskov <rasmusst@metropolia.fi>
2018-12-03 16:09:25 +0200

    primitive recipe addition.

--- app/src/main/java/com/example/werefrogs/cakeulator/MainActivity.java
+++ app/src/main/java/com/example/werefrogs/cakeulator/MainActivity.java
@@ -3,16 +3,26 @@ package com.example.werefrogs.cakeulator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText newName, newAmount, newUnit, newIngredient;
+    Recipe newRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        newName = findViewById(R.id.et_addName);
+        newAmount = findViewById(R.id.et_addAmount);
+        newUnit = findViewById(R.id.et_addUnit);
+        newIngredient = findViewById(R.id.et_addIngredient);
 
+        newRecipe = new Recipe();
 
     }
     public void buttonPressed_toLibrary(View v) {
@@ -25,5 +35,18 @@ public class MainActivity extends AppCompatActivity {
         r1.addIngredient(new Ingredient(2, "slice", "roast beef"));
         r1.addIngredient(new Ingredient(4, "slice", "cucumber"));
         RecipeList.getInstance().addRecipe(r1);
+
+        String nameToAdd = newName.getText().toString();
+        Log.d("setName", nameToAdd );
+        newRecipe.setName(nameToAdd);
+        Log.d("getName", newRecipe.getName());
+        RecipeList.getInstance().addRecipe(newRecipe);
+    }
+    public void buttonPressed_addIngredient(View v) {
+        int amountToAdd = Integer.parseInt(newAmount.getText().toString());
+        String unitToAdd = newUnit.getText().toString();
+        String ingredientToAdd = newIngredient.getText().toString();
+
+        newRecipe.addIngredient(new Ingredient(amountToAdd, unitToAdd, ingredientToAdd));
     }
 }
