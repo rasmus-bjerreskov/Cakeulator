8a3c49607ef0863ae812d9429d41ba9693450f1b
Elizabeth <elizabec@metropolia.fi>
2018-12-05 09:45:12 +0200

    Comments and sources added (about Splash screen and search function)

--- app/src/main/java/com/example/werefrogs/cakeulator/SplashActivity_Sleep.java
+++ app/src/main/java/com/example/werefrogs/cakeulator/SplashActivity_Sleep.java
@@ -9,3 +9,4 @@ public class SplashActivity_Sleep extends Application {
         SystemClock.sleep(1000);
     }
 }
+// Splash screen was created using the tutorial from https://medium.com/@ssaurel/create-a-splash-screen-on-android-the-right-way-93d6fb444857
